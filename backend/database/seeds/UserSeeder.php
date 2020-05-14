82f4ed8c7ead0e1eb413e84f09bc064742bfc47a
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2020-05-14 00:00:12 -0300

    Adicionando Seed

--- backend/database/seeds/UserSeeder.php
+++ backend/database/seeds/UserSeeder.php
@@ -11,6 +11,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => "Teste Seeder",
+            'email' => 'seeder@teste.com',
+            'password' => Hash::make('12345'),
+        ]);
     }
 }
